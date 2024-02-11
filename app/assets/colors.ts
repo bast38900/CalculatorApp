00a73ee70b1ec72186cedcbb20ab45ba7f1bead3
Bast38900 <bast38900@edu.ucl.dk>
2024-02-11 13:26:32 +0100

    template created

---
+++ app/assets/colors.ts
@@ -0,0 +1,6 @@
+export const Colors = {
+  black: '#000000',
+  orange: '#FFA500',
+  white: '#FFFFFF',
+  grey: '#808080'
+};
---
+++ app/screens/calculator.screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+type Props = {}
+
+export const CalculatorScreen = (props: Props) => {
+  return (
+    <Text style={{color: "white", fontSize: 22}}>Calculator Screen</Text>
+  )
+}
