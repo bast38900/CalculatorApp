2c3db8ee8a6244d8689ffbd8a51935d67df27cd6
Bast38900 <bast38900@edu.ucl.dk>
2024-02-13 10:35:01 +0100

    main layout done

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,5 +1,6 @@
 import { StyleProp, StyleSheet, Text, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native'
 import React from 'react'
+import { Colors } from '../assets';
 
 type Props = {
   title: string;
@@ -22,8 +23,12 @@ export const Button = (props: Props) => {
 const styles = StyleSheet.create({
   button: {
     justifyContent: 'center',
+    flex: 1,
+    borderWidth: 1,
+    borderColor: Colors.black
 },
 title: {
     textAlign: 'center',
+    fontSize: 28,
 },
 })
--- app/screens/calculator.screen.tsx
+++ app/screens/calculator.screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { Button } from '../components/button'
 import { Colors } from '../assets'
 
@@ -7,9 +7,47 @@ type Props = {}
 
 export const CalculatorScreen = (props: Props) => {
   return (
-    <>
-      <Text style={{color: "white", fontSize: 22}}>Calculator Screen</Text>
-      <Button title='+' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
-    </>
+    <View style={styles.columnContainer}>
+      <View style={{flex: 2, justifyContent: 'center', alignSelf: 'flex-end'}}>
+        <Text style={{color: "white", fontSize: 42, fontWeight: 'bold' , alignSelf: 'center'}}>Calculator</Text>
+      </View>
+      <View style={styles.rowContainer}>
+        <Button title='7' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='8' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='9' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='÷' style={{backgroundColor: Colors.orange}} textStyle={{color: Colors.white}}/>
+      </View>
+      <View style={styles.rowContainer}>
+        <Button title='4' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='5' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='6' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='x' style={{backgroundColor: Colors.orange}} textStyle={{color: Colors.white}}/>
+      </View>
+      <View style={styles.rowContainer}>
+        <Button title='1' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='2' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='3' style={{backgroundColor: Colors.white}} textStyle={{color: Colors.black}}/>
+        <Button title='-' style={{backgroundColor: Colors.orange}} textStyle={{color: Colors.white}}/>
+      </View>
+      <View style={styles.rowContainer}>
+        <Button title='0' style={{backgroundColor: Colors.white, flex: 3}} textStyle={{color: Colors.black}}/>
+        <Button title='+' style={{backgroundColor: Colors.orange}} textStyle={{color: Colors.white}}/>
+      </View>
+      <View style={styles.rowContainer}>
+        <Button title='C' style={{backgroundColor: Colors.grey}} textStyle={{color: Colors.white}}/>
+        <Button title='=' style={{backgroundColor: Colors.orange, flex: 3}} textStyle={{color: Colors.white}}/>
+      </View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  columnContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  rowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+})
